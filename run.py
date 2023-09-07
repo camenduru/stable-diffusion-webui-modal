30533d341357e38899464516e4c85b1974a01b10
camenduru <54370274+camenduru@users.noreply.github.com>
2023-09-07 09:13:32 +0300

    t

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ volume = modal.NetworkFileSystem.new().persisted("stable-diffusion-webui")
     modal.Image.from_registry("nvidia/cuda:12.2.0-base-ubuntu22.04", add_python="3.11")
     .run_commands(
         "apt-get update -y && \
-        apt-get install -y aria2 libgl1 libglib2.0-0 wget git git-lfs && \
+        apt-get install -y git git-lfs aria2 libgl1 libglib2.0-0 wget && \
         pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 && \
         pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1"
     ),
@@ -21,6 +21,7 @@ async def run():
     os.chdir(f"/content/stable-diffusion-webui")
     os.system(f"git lfs install")
     os.system(f"git reset --hard")
+    os.system(f"sed -i 's/--refetch //g' /content/stable-diffusion-webui/modules/launch_utils.py")
     os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/counterfeit-xl/resolve/main/counterfeitxl_v10.safetensors -d /content/stable-diffusion-webui/models/Stable-diffusion -o counterfeitxl_v10.safetensors")
     os.system(f"python launch.py --cors-allow-origins=* --xformers --theme dark --gradio-queue")
 

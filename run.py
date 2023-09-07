379daee79e0c26e49268fc649d4864817d507aa6
camenduru <54370274+camenduru@users.noreply.github.com>
2023-09-07 04:41:29 +0300

    test

--- run.py
+++ run.py
@@ -1,1 +1,29 @@
 import modal
+import os
+
+stub = modal.Stub("stable-diffusion-webui")
+volume = modal.NetworkFileSystem.new().persisted("stable-diffusion-webui")
+
+@stub.function(
+    image=modal.Image.from_dockerhub("nvidia/cuda:12.2.0-base-ubuntu22.04")
+    .run_commands(
+        "apt-get update -y",
+        "apt-get install -y aria2 libgl1 libglib2.0-0 wget git git-lfs python3-pip python-is-python3",
+        "pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118",
+        "pip install xformers==0.0.20 triton==2.0.0 packaging==23.1"
+    )
+    network_file_systems={"/content/stable-diffusion-webui": volume},
+    gpu="T4",
+    timeout=6000,
+)
+async def run():
+    os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
+    os.chdir(f"/home/user/app/stable-diffusion-webui")
+    os.system(f"git lfs install")
+    os.system(f"git reset --hard")
+    os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/counterfeit-xl/resolve/main/counterfeitxl_v10.safetensors -d /content/stable-diffusion-webui/models/Stable-diffusion -o counterfeitxl_v10.safetensors")
+    os.system(f"python launch.py --cors-allow-origins=* --xformers --theme dark --gradio-queue")
+
+@stub.local_entrypoint()
+def main():
+    run.call()
